61d01bfa853553edbf033f5e675c4b1d70851a17
masud1989 <mranasrj@gmail.com>
2022-01-26 14:21:17 +0600

    semi-full

---
+++ stringadd.js
@@ -0,0 +1,17 @@
+
+// String Adding 
+// ======================================== 
+
+var first = 'Masud';
+var last = 'Rana';
+var fullName = first + ' ' + last;
+console.log('Your Name is:' + ' ' + fullName);
+
+// var price1 ='20';
+// var price2 ='21'; 
+var price1 ='20';
+var price2 ='21'; 
+var priceNum1=parseInt(price1);
+var priceNum2=parseInt(price2);
+var priceTotal =priceNum1 + priceNum2;
+console.log(priceTotal);
