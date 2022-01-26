07c9bcfd09bb3182ad0f391fb6ac0df44e5d603a
masud1989 <mranasrj@gmail.com>
2022-01-26 14:01:16 +0600

    first module

---
+++ 1st.js
@@ -0,0 +1,64 @@
+var price = 200;
+var age = 20;
+var name = 'Hero Alan'
+var condi = true;
+console.log (condi);
+
+
+// Declarin Variable
+// ==================================================
+
+// 1. Reserved Keyword not allowed
+// 2. Name Should be single word 
+// 3. Name Should be started with number (var 123number not allowed) 
+
+// Mathemetical Operators Example========================================
+var onionPrice = 50;
+var eggPrice = 40;
+var eggQuantity=10;
+console.log(onionPrice);
+console.log(eggPrice);
+// Additon 
+console.log(onionPrice+eggPrice);
+var totalPrice = onionPrice+eggPrice;
+// substuction
+var diffrencePrice = onionPrice-eggPrice;
+console.log(totalPrice);
+console.log(diffrencePrice);
+// Multiplication
+var totalEggPrice =  eggQuantity*eggPrice;
+console.log(totalEggPrice);
+
+// Division
+var totalEggPrice = 400;
+var eggQuantity = 10;
+var unitPrice = totalEggPrice/eggQuantity;
+console.log('Unit Price of Egg is:' + unitPrice);
+
+
+// Math Operation
+// ====================================================
+
+var price1 = 31;
+var price2 = 10;
+var sum = price1+price2;
+var difference = price1-price2;
+var division = price1/price2;
+var mult = price1*price2;
+
+console.log(sum);
+console.log(difference);
+console.log(division);
+console.log(mult);
+
+// short hand 
+// price1 = price1+10;
+// price1 +=10;
+// price1 -=10;
+// price1++
+var love = 100;
+// love-=1;
+love--;
+console.log(love);
+
+
