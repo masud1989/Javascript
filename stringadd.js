b8f06f7c3a33938d7bf9baa65ff512190008906b
masud1989 <mranasrj@gmail.com>
2022-01-26 19:03:37 +0600

    OK

--- stringadd.js
+++ stringadd.js
@@ -5,7 +5,7 @@
 var first = 'Masud';
 var last = 'Rana';
 var fullName = first + ' ' + last;
-console.log('Your Name is:' + ' ' + fullName);
+
 
 // var price1 ='20';
 // var price2 ='21'; 
@@ -14,4 +14,19 @@ var price2 ='21';
 var priceNum1=parseInt(price1);
 var priceNum2=parseInt(price2);
 var priceTotal =priceNum1 + priceNum2;
-console.log(priceTotal);
+var isRaining = true;
+// console.log('Your Name is:' + ' ' + fullName);
+// console.log(typeof priceTotal);
+// console.log(isRaining)
+
+
+
+// Special Edition ====================================
+var num1 = 0.1;
+var num2 = 0.2;
+var total = num1+num2;
+console.log(total.toFixed(2));
+
+var num1=99.5;
+var num2=15;
+console.log(num1+num2);
