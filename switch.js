cf6022745abd451ee3b342e084f1e0f22006e076
masud1989 <mranasrj@gmail.com>
2022-01-29 16:09:49 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,58 @@
+// var color = 'white';
+
+// if (color == 'blue'){
+//     console.log('Color is : Blue');
+// }
+
+// else if(color == 'green'){
+//     console.log('Color is : Green');
+// }
+// else if(color == 'yellow'){
+//     console.log('Color is : Yellow');
+// }
+// else if(color == 'red'){
+//     console.log('Color is : Red');
+// }
+// else if(color == 'white'){
+//     console.log('Color is : White');
+// }
+// else if(color == 'gray'){
+//     console.log('Color is : Gray');
+// }
+// else if(color == 'orange'){
+//     console.log('Color is : Orange');
+// }
+// else if(color == 'magenta'){
+//     console.log('Color is : Magenta');
+// }
+// else{
+//     console.log('Color is : Black');
+// } 
+
+var color = '';
+
+switch (color){
+    case 'blue':
+        console.log('Color is : Blue')
+        break;
+    case 'green':
+        console.log('Color is : green')
+        break;
+    case 'yellow':
+        console.log('Color is : yellow')
+        break;
+    case 'gray':
+        console.log('Color is : gray')
+        break;
+    case 'orange':
+        console.log('Color is : Orange')
+        break;
+    case 'white':
+        console.log('Color is : White')
+        break;
+
+    default:
+        console.log('Color is: Black')
+}
+
+
