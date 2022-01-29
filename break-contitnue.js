5d75d515973405a5881c36e0a4f937dde9078e69
masud1989 <mranasrj@gmail.com>
2022-01-29 16:48:12 +0600

    break-continue

---
+++ break-contitnue.js
@@ -0,0 +1,24 @@
+// var i = 0;
+// while (i<10){
+    //     if (i == 8){
+    //         break;
+    //     }
+// console.log(i);
+//     i++;
+// }
+
+    // for (var i = 0; i<=25; i++){
+    //     console.log (i);
+    //             if (i == 15){
+    //                 break;
+    //             }
+    // }
+
+    var numbers = [54, 65, 59, 76, 88];
+    for (var i = 0; i < numbers.length; i++){
+        var number = numbers[i];
+        console.log(number);
+        if (number > 70){
+            break;
+        }
+    }
