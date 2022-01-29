b17bb6e678237f424acb801926468c2626fb2974
masud1989 <mranasrj@gmail.com>
2022-01-29 15:09:01 +0600

    function with multiparameter

---
+++ multi-paramiter.js
@@ -0,0 +1,25 @@
+// function addTwoNumbers(number1, number2){
+//     console.log(number1, number2);
+// }
+// addTwoNumbers (21, 59);
+// function addTwoNumbers(number1, number2){
+//     console.log(number1, number2);
+//     var total = number1 + number2;
+//     return total;
+// }
+
+// firstNumber = 35;
+// secondNumber = 25;
+// var result = addTwoNumbers(firstNumber, secondNumber);
+// console.log('The Results is: ',result );
+
+function multTwoNumbers(number1, number2){
+    console.log(number1, number2);
+    var mult = number1 * number2;
+    return mult;
+}
+
+firstNumber = 10;
+secondNumber = 5;
+var result = multTwoNumbers(firstNumber, secondNumber);
+console.log('The Results is: ',result );
