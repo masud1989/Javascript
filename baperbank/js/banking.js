95242761eb53fad98410b4a18ecbc91db97c2933
masud1989 <mranasrj@gmail.com>
2022-02-10 17:46:13 +0600

    banking

--- baperbank/js/banking.js
+++ baperbank/js/banking.js
@@ -1,14 +1,11 @@
-//Login Related 
-// ============================================================== 
-document.getElementById('login-btn').addEventListener('click', function(){
-    // get user email 
-    const emailField = document.getElementById('user-email');
-    const userEmail = emailField.value;
-    // get user password 
-    const passwordField = document.getElementById('user-password');
-    const userPassword = passwordField.value;
-    //check email
-    if(userEmail == 'user@gmail.com' && userPassword == 'secret'){
-        window.location.href = 'banking.html';
-    }
+// Handle Deposite Event 
+// =========================================================================
+document.getElementById('deposite-btn').addEventListener('click', function(){
+                                                         // console.log('button clicked..')
+    const depositeInput = document.getElementById('deposite-amount');
+    const depositeAmount = depositeInput.value;
+                                                         // console.log(depositeAmount);
+    const depositeTotal = document.getElementById('deposite-total');
+    depositeTotal.innerText = depositeAmount;
+    depositeInput.value = '';
 })
