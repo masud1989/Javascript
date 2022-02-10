1e4d0cbcbebaeed1d7d3a774fdd6b3c2a88f910d
masud1989 <mranasrj@gmail.com>
2022-02-10 17:22:29 +0600

    banking

---
+++ baperbank/js/banking.js
@@ -0,0 +1,14 @@
+//Login Related 
+// ============================================================== 
+document.getElementById('login-btn').addEventListener('click', function(){
+    // get user email 
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password 
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    //check email
+    if(userEmail == 'user@gmail.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html';
+    }
+})
