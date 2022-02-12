a9c4e38247e39203a185ee66ac78c9c53e9a8b8c
masud1989 <mranasrj@gmail.com>
2022-02-12 13:31:33 +0600

    total-function-using-negative withdraw full and final

--- baperbank-functional/js/banking.js
+++ baperbank-functional/js/banking.js
@@ -15,10 +15,18 @@ function updateTotalField(totalFieldId, inputAmount){
     amountExist.innerText = inputAmount + existAmount;
 }
 
-function updateBalance(depositeAmount, isDeposite){
+function getCurrentBalance(){
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const balanceTotalExist = parseFloat(balanceTotalText);
+    return balanceTotalExist;
+}
+
+function updateBalance(depositeAmount, isDeposite){
+     const balanceTotal = document.getElementById('balance-total');
+    // const balanceTotalText = balanceTotal.innerText;
+    // const balanceTotalExist = parseFloat(balanceTotalText);
+    const previousBalanceTotal = getCurrentBalance();
     if(isDeposite == true){
         balanceTotal.innerText = balanceTotalExist + depositeAmount; 
     }
@@ -30,17 +38,30 @@ function updateBalance(depositeAmount, isDeposite){
 // deposite option 
 document.getElementById('deposite-btn').addEventListener('click', function(){
     const depositeAmount = getInputValue('deposite-amount');
-    updateTotalField('deposite-total', depositeAmount);
-    // update balance 
-    updateBalance(depositeAmount, true);
+
+    if(depositeAmount > 0){
+        updateTotalField('deposite-total', depositeAmount);
+        // update balance 
+        updateBalance(depositeAmount, true);
+    }
+    else{
+        alert("Please Input Positive Number..");
+    }
 });
 
 
 // withdraw option 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withDrawAmount = getInputValue('withdraw-amount');
-    updateTotalField('withdraw-total', withDrawAmount);
+    const currentBalace = getCurrentBalance();
+
+    if(withDrawAmount > 0 && withDrawAmount < currentBalace ){
+        updateTotalField('withdraw-total', withDrawAmount);
     // update balance 
     const balanceTotal = document.getElementById('balance-total');
     updateBalance(withDrawAmount, false);
+    }
+    else{
+        alert("Please Input Positive Number lower than Your Balance..");
+    }
 })
