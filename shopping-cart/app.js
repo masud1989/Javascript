391ca65efaeedd8e1c94efe72cff5defd3afff43
masud1989 <mranasrj@gmail.com>
2022-02-13 14:54:10 +0600

    caseTotal

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,13 +1,17 @@
 // console.log('I am in a separate file')
 function updateCaseQuatity(isPlus){
     const caseInput = document.getElementById('case-quantity');
-    const caseQuantity = parseInt(caseInput.value);
-    if(isPlus){
-        caseInput.value = caseQuantity + 1;
+    let caseQuantity = caseInput.value;
+    if(isPlus == true){
+        caseQuantity = parseInt(caseQuantity) + 1;
     } 
-    else{
-        caseInput.value = caseQuantity - 1;
+    else if(caseQuantity > 0){
+        caseQuantity = parseInt(caseQuantity) - 1;
     }
+    // update case Total
+    caseInput.value = caseQuantity;
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseQuantity * 59;
     
 }
 
